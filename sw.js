2e8e411d9a7eed2f75a4a4d26d9a67259d337bc9
Mohammed hussein <mohammedhussein1225m@gmail.com>
2025-03-13 14:44:37 +0200

    Fixed error path  file

--- sw.js
+++ sw.js
@@ -1,25 +1,23 @@
 const CACHE_NAME = "dashboard-cache-v1";
 const FILES_TO_CACHE = [
-  "dashboard_cachaing/sw.js",
-  "dashboard_cachaing/manifest.json",
-  "dashboard_cachaing/index.html",
-  "dashboard_cachaing/login.html",
-  "dashboard_cachaing/show.html",
-  "dashboard_cachaing/Add.html",
-  "dashboard_cachaing/css/style.css",
-  "dashboard_cachaing/css/all.min.css",
-  "dashboard_cachaing/js/main.js",
-  "dashboard_cachaing/js/all.min.js",
-  "dashboard_cachaing/image/avatar.png",
-  "dashboard_cachaing/image/icon.png",
-  "dashboard_cachaing/image/tiger.jpg",
-  "dashboard_cachaing/image/user.webp",
-  "dashboard_cachaing/webfonts/fa-brands-400.ttf",
-  "dashboard_cachaing/webfonts/fa-regular-400.woff2",
-  "dashboard_cachaing/webfonts/fa-solid-900.ttf",
-  "dashboard_cachaing/webfonts/fa-regular-400.ttf",
+  "/dashboard_cachaing/manifest.json",
+  "/dashboard_cachaing/index.html",
+  "/dashboard_cachaing/login.html",
+  "/dashboard_cachaing/show.html",
+  "/dashboard_cachaing/Add.html",
+  "/dashboard_cachaing/css/style.css",
+  "/dashboard_cachaing/css/all.min.css",
+  "/dashboard_cachaing/js/main.js",
+  "/dashboard_cachaing/js/all.min.js",
+  "/dashboard_cachaing/image/avatar.png",
+  "/dashboard_cachaing/image/icon.png",
+  "/dashboard_cachaing/image/tiger.jpg",
+  "/dashboard_cachaing/image/user.webp",
+  "/dashboard_cachaing/webfonts/fa-brands-400.ttf",
+  "/dashboard_cachaing/webfonts/fa-regular-400.woff2",
+  "/dashboard_cachaing/webfonts/fa-solid-900.ttf",
+  "/dashboard_cachaing/webfonts/fa-regular-400.ttf",
 ];
-
 // تثبيت Service Worker وتخزين الملفات
 self.addEventListener("install", (event) => {
   event.waitUntil(
